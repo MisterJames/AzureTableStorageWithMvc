7cd3dff2e19b55c1ce5d5ebec3853d8f17bc22a0
James Chambers <james@jameschambers.com>
2015-07-18 10:08:57 -0500

    Completed implementation on Day 3 exercises
    - added Ninject package
    - added KittehRepository and implemented interface
    - updated/simplified controller

--- AzureTableStorageWithMvc/Controllers/HomeController.cs
+++ AzureTableStorageWithMvc/Controllers/HomeController.cs
@@ -12,57 +12,24 @@ namespace AzureTableStorageWithMvc.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ITableStorageRepository<KittehEntity> _kittehRepository;
+
+        public HomeController(ITableStorageRepository<KittehEntity> kittehRepository)
+        {
+            _kittehRepository = kittehRepository;
+        }
+
         public ActionResult Index()
         {
-            var storageAccount = CloudStorageAccount.Parse(
-                CloudConfigurationManager.GetSetting("StorageConnectionString"));
-
-            var client = storageAccount.CreateCloudTableClient();
-
-            var kittehTable = client.GetTableReference("PicturesOfKittehs");
-            if (!kittehTable.Exists())
-            {
-                kittehTable.Create();
-
-                var thrillerKitteh = new KittehEntity("FunnyKittehs", "ThrillerKitteh");
-                thrillerKitteh.ImageUrl = "http://cdn.buzznet.com/assets/users16/crizz/default/funny-pictures-thriller-kitten-impresses--large-msg-121404159787.jpg";
-
-                var pumpkinKitteh = new KittehEntity("FunnyKittehs", "PumpkinKitteh");
-                pumpkinKitteh.ImageUrl = "http://rubmint.com/wp-content/plugins/wp-o-matic/cache/6cb1b_funny-pictures-colur-blind-kitteh-finded-yew-a-pumikin.jpg";
-
-                var batchOperation = new TableBatchOperation();
-
-                batchOperation.Insert(thrillerKitteh);
-                batchOperation.Insert(pumpkinKitteh);
-                kittehTable.ExecuteBatch(batchOperation);
-
-            }
-
-            var kittehQuery = new TableQuery<KittehEntity>()
-                .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "FunnyKittehs"));
-
-            var kittehs = kittehTable.ExecuteQuery(kittehQuery).ToList();
-
+            var kittehs = _kittehRepository.GetByPartition("FunnyKittehs");
             return View(kittehs);
         }
 
         [HttpPost]
         public ActionResult Index(KittehEntity entity)
         {
-            var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
-
-            var client = storageAccount.CreateCloudTableClient();
-            var kittehTable = client.GetTableReference("PicturesOfKittehs");
-
-            var insert = TableOperation.Insert(entity);
-            kittehTable.Execute(insert);
-
-            var kittehQuery = new TableQuery<KittehEntity>()
-                .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "FunnyKittehs"));
-
-            var kittehs = kittehTable.ExecuteQuery(kittehQuery).ToList();
-
-            return View(kittehs);
+            _kittehRepository.Insert(entity);
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
---
+++ AzureTableStorageWithMvc/Repositories/KittehRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AzureTableStorageWithMvc.Models;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureTableStorageWithMvc.Repositories
+{
+    public class KittehRepository : ITableStorageRepository<KittehEntity>
+    {
+        private readonly CloudTableClient _client;
+
+        public KittehRepository()
+        {
+            var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            _client = storageAccount.CreateCloudTableClient();
+        }
+
+        public void Insert(KittehEntity entity)
+        {
+            var kittehTable = _client.GetTableReference("PicturesOfKittehs");
+            var insert = TableOperation.Insert(entity);
+            kittehTable.Execute(insert);
+        }
+
+        public void Update(KittehEntity entity)
+        {
+            var kittehTable = _client.GetTableReference("PicturesOfKittehs");
+            var insert = TableOperation.Replace(entity);
+            kittehTable.Execute(insert);
+        }
+
+        public void Delete(KittehEntity entity)
+        {
+            var kittehTable = _client.GetTableReference("PicturesOfKittehs");
+            var insert = TableOperation.Delete(entity);
+            kittehTable.Execute(insert);
+        }
+
+        public IEnumerable<KittehEntity> GetByPartition(string partitionKey)
+        {
+            var kittehTable = _client.GetTableReference("PicturesOfKittehs");
+            var kittehQuery = new TableQuery<KittehEntity>()
+                            .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, partitionKey));
+            var kittehs = kittehTable.ExecuteQuery(kittehQuery).ToList();
+
+            return kittehs;
+        }
+    }
+}
